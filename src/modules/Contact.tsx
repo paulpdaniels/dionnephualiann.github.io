cebed26ca72f8c7dbe1a4a806224d0fe2b87f4cc
paulpdaniels <paulpdaniels@gmail.com>
2016-10-27 00:32:04 -0700

    Added new images. Improved build and bundle size. Fixed carousel.

--- src/modules/Contact.tsx
+++ src/modules/Contact.tsx
@@ -1,47 +1,50 @@
 'use strict';
-import {Form, FormGroup, Col, FormControl, Button} from 'react-bootstrap';
+import {Form, FormGroup, Col, FormControl, Button, InputGroup} from 'react-bootstrap';
 
 export const Contact = ({route}) => (
-    <div>
-        <div className="contactForm">
-            <Form horizontal>
-                <FormGroup>
-                    <Col sm={6}>
-                        <FormControl type="text" required placeholder="Name"/>
-                    </Col>
-                    <Col sm={6}>
-                        <FormControl type="email" required placeholder="Email"/>
-                    </Col>
-                </FormGroup>
-                <FormGroup>
-                    <Col xs={12}>
-                        <FormControl componentClass="textarea"
-                                     placeholder="Message"
-                                     rows={8}
-                                     required={true}
-                        />
-                    </Col>
-                </FormGroup>
-                <FormGroup>
-                    <Col xs={2}>
-                        <Button bsStyle="info">Submit</Button>
-                    </Col>
-                </FormGroup>
-            </Form>
-        </div>
-        <div className="contactDetails">
-            <div id="address" className="block">
-                <h4>Address</h4>
-                <p>{route.info.address}</p>
-            </div>
-            <div id="phoneNumber" className="block">
-                <h4>Phone Number</h4>
-                <p>{route.info.phoneNumber}</p>
-            </div>
-            <div id="emailAddress" className="block">
-                <h4>Email Address</h4>
-                <p>{route.info.emailAddress}</p>
-            </div>
-        </div>
+  <Col xs={12}>
+    <div className="contactForm">
+      <Form horizontal>
+        <FormGroup>
+          <Col sm={6}>
+            <FormControl type="text" required placeholder="Name"/>
+          </Col>
+          <Col sm={6}>
+            <InputGroup>
+              <InputGroup.Addon>@</InputGroup.Addon>
+              <FormControl type="email" required placeholder="Email"/>
+            </InputGroup>
+          </Col>
+        </FormGroup>
+        <FormGroup>
+          <Col xs={12}>
+            <FormControl componentClass="textarea"
+                         placeholder="Message"
+                         rows={8}
+                         required={true}
+            />
+          </Col>
+        </FormGroup>
+        <FormGroup>
+          <Col xs={2}>
+            <Button bsStyle="info">Submit</Button>
+          </Col>
+        </FormGroup>
+      </Form>
     </div>
+    <div className="contactDetails">
+      <div id="address" className="block">
+        <h4>Address</h4>
+        <p>{route.info.address}</p>
+      </div>
+      <div id="phoneNumber" className="block">
+        <h4>Phone Number</h4>
+        <p>{route.info.phoneNumber}</p>
+      </div>
+      <div id="emailAddress" className="block">
+        <h4>Email Address</h4>
+        <p>{route.info.emailAddress}</p>
+      </div>
+    </div>
+  </Col>
 );
