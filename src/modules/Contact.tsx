fa218f9c6d207603ea361334ace159fd0de97002
paulpdaniels <paulpdaniels@gmail.com>
2016-10-28 01:27:07 -0700

    Fixed low res images, added sloth, added resume

--- src/modules/Contact.tsx
+++ src/modules/Contact.tsx
@@ -1,50 +1,53 @@
 'use strict';
-import {Form, FormGroup, Col, FormControl, Button, InputGroup} from 'react-bootstrap';
+import {Form, FormGroup, Col, FormControl, ControlLabel, Button, InputGroup} from 'react-bootstrap';
 
 export const Contact = ({route}) => (
-  <Col xs={12}>
-    <div className="contactForm">
-      <Form horizontal>
-        <FormGroup>
-          <Col sm={6}>
-            <FormControl type="text" required placeholder="Name"/>
-          </Col>
-          <Col sm={6}>
-            <InputGroup>
-              <InputGroup.Addon>@</InputGroup.Addon>
-              <FormControl type="email" required placeholder="Email"/>
-            </InputGroup>
-          </Col>
+  <Col xs={10} xsOffset={1}>
+    {/*<div className="contactForm">
+     <Form horizontal>
+     <FormGroup>
+     <Col sm={6}>
+     <FormControl type="text" required placeholder="Name"/>
+     </Col>
+     <Col sm={6}>
+     <InputGroup>
+     <InputGroup.Addon>@</InputGroup.Addon>
+     <FormControl type="email" required placeholder="Email"/>
+     </InputGroup>
+     </Col>
+     </FormGroup>
+     <FormGroup>
+     <Col xs={12}>
+     <FormControl componentClass="textarea"
+     placeholder="Message"
+     rows={8}
+     required={true}
+     />
+     </Col>
+     </FormGroup>
+     <FormGroup>
+     <Col xs={2}>
+     <Button bsStyle="info">Submit</Button>
+     </Col>
+     </FormGroup>
+     </Form>
+     </div>*/}
+    <Form method="get" action="/extras/DionnePhuaResume.pdf">
+      <div className="contactDetails">
+        <FormGroup id="address" className="block">
+          <ControlLabel>Address</ControlLabel>
+          <FormControl.Static>{route.info.address}</FormControl.Static>
+        </FormGroup>
+        <FormGroup id="emailAddress" className="block">
+          <ControlLabel>Email Address</ControlLabel>
+          <FormControl.Static>
+            <a href={`mailto:${route.info.emailAddress}`}>{route.info.emailAddress}</a>
+          </FormControl.Static>
         </FormGroup>
         <FormGroup>
-          <Col xs={12}>
-            <FormControl componentClass="textarea"
-                         placeholder="Message"
-                         rows={8}
-                         required={true}
-            />
-          </Col>
+          <Button type="submit">Download Resume</Button>
         </FormGroup>
-        <FormGroup>
-          <Col xs={2}>
-            <Button bsStyle="info">Submit</Button>
-          </Col>
-        </FormGroup>
-      </Form>
-    </div>
-    <div className="contactDetails">
-      <div id="address" className="block">
-        <h4>Address</h4>
-        <p>{route.info.address}</p>
       </div>
-      <div id="phoneNumber" className="block">
-        <h4>Phone Number</h4>
-        <p>{route.info.phoneNumber}</p>
-      </div>
-      <div id="emailAddress" className="block">
-        <h4>Email Address</h4>
-        <p><a href={`mailto:${route.info.emailAddress}`}>{route.info.emailAddress}</a></p>
-      </div>
-    </div>
+    </Form>
   </Col>
 );
